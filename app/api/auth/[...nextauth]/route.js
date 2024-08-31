9173fb1b6e14ca08dfd8b177240d42fa37d6ce6a
MD ASHIKUR RAHMAN ASHIK <md.ashikur.upwork@gmail.com>
2024-08-31 11:48:47 +0000

    Setting up providers

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -6,8 +6,8 @@ import User from "@models/user";
 const handler = NextAuth({
   providers: [
     GoogleProvider({
-      clientId: process.env.GOOGLE_ID,
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+      clientId: process.env.NEXT_PUBLIC_GOOGLE_ID,
+      clientSecret: process.env.NEXT_PUBLIC_GOOGLE_CLIENT_SECRET,
     }),
   ],
   async session({ session }) {
